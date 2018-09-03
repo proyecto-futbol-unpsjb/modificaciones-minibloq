d41fa42cad87c5bcc68055d40f761358df81a737
fepz <franpaez@gmail.com>
2018-09-03 10:16:19 -0300

    Bloque para Control Remoto.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Modificaciones Minibloq
-Agregados y modificaciones realizadas a Minibloq
+Agregados y modificaciones realizadas a Minibloq.
+
+## Instalación
+Por ahora, copiar y pegar en una instalación de Minibloq para agregar/reemplazar los componentes.
---
+++ libs/Arduino/IRRemote/v1.2/IRremoteRemoteControlCodes.h
@@ -0,0 +1,29 @@
+#ifndef IRremoteRemoteControlCodes_h
+#define IRremoteRemoteControlCodes_h
+
+#define BOTON_1         1
+#define BOTON_2         2
+#define BOTON_3         3
+#define BOTON_4         4
+#define BOTON_5         5
+#define BOTON_6         6
+#define BOTON_7         7
+#define BOTON_8         8
+#define BOTON_9         9
+#define BOTON_0         10
+#define BOTON_POWER     11
+#define BOTON_MUTE      12
+#define BOTON_OSD       13
+#define BOTON_DERECHA   14
+#define BOTON_IZQUIERDA 15
+#define BOTON_ARRIBA    16
+#define BOTON_ABAJO     17
+#define BOTON_ACH       18
+#define BOTON_3RAYAS    19
+#define BOTON_III       20
+#define BOTON_TIMER     21
+#define BOTON_PANTALLA1 22
+#define BOTON_PANTALLA2 23
+#define BOTON_AV        24
+
+#endif
